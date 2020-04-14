f81601a3f61683c1b18a4a0a491b9ee2dba3708c
Joshua Okobia <joshuaokobia@Joshuas-MacBook-Pro.local>
2020-04-14 22:59:35 +0100

    logs endpoint finished

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import xmlParser from 'express-xml-bodyparser';
 import routes from './routes';
+import logRequestDetails from './middlewares';
 
 const app = express();
 app.use(cors());
 app.use(xmlParser());
 app.use(bodyParser.json());
+app.use(logRequestDetails);
 
 app.use('/api/v1/on-covid-19', routes);
 
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,23 @@
 import { Router } from 'express';
 import jtx from 'jtx';
+import fs from 'fs';
 import covid19ImpactEstimator from './estimator';
 
 const router = new Router();
+
+router.get('/logs', (req, res) => {
+  try {
+    res.header('Content-Type', 'plain/text');
+    fs.readFile('log.txt', (err, buf) => {
+      if (err) {
+        throw err;
+      }
+      return res.send(buf);
+    });
+  } catch (err) {
+    return res.err({ status: 'error', message: err.message });
+  }
+});
 
 router.post('/', (req, res) => {
   try {
